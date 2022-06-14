1d59cfad299c55dfcb354ae43e0cfceefac7c1d5
Augusto <jesus_augga_99@icloud.com>
2022-06-14 12:45:54 -0500

    refactor nameable class

--- Classes/nameable.rb
+++ Classes/nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-  def correct_name(name)
-    name.capitalize.slice(0, 10)
+  def correct_name
+    raise NotImplementedError, 'Please implement correct_name'
   end
 end
--- Classes/person.rb
+++ Classes/person.rb
@@ -1,14 +1,16 @@
 require 'securerandom'
+require_relative 'nameable'
 
 class Person
   attr_accessor :name, :age, :parent_permission
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = SecureRandom.uuid
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @nameable = Nameable.new
   end
 
   private
@@ -22,4 +24,15 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @nameable.correct_name(name)
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
