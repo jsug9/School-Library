692fb1e7b99353a0c925a47c9532fb79eb0a5e1a
Augusto <jesus_augga_99@icloud.com>
2022-06-14 15:08:20 -0500

    fix linter errors

--- Classes/person.rb
+++ Classes/person.rb
@@ -7,7 +7,8 @@ class Person < Nameable
   attr_accessor :name, :age, :parent_permission
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = SecureRandom.uuid
     @name = name
     @age = age
@@ -33,10 +34,10 @@ person = Person.new(22, 'maximilianus')
 puts person.name
 # => maximilianus
 
-capitalizedPerson = CapitalizeDecorator.new(person)
-puts capitalizedPerson.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
 # => Maximilianus
 
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-puts capitalizedTrimmedPerson.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
 # => Maximilian
