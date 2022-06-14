a2604045b8f75be7a76b6d455dd01c8b212d9fc5
Augusto <jesus_augga_99@icloud.com>
2022-06-14 16:49:10 -0500

    add associations between rental and book

--- Classes/rental.rb
+++ Classes/rental.rb
@@ -1,7 +1,20 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, :person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    @person = person
+  end
+
+  def addBook(book)
+    @book = book
+    @book.rentals << self unless @book.rentals.include?(self)
+  end
+
+  def addPerson(person)
+    @person = person
+    @person.rentals.push << self unless @person.rentals.include?(self)
   end
 end
