f8289bccc03bf8921e916be23a71612b7f8fa65c
Augusto <jesus_augga_99@icloud.com>
2022-06-14 09:59:13 -0500

    fix linter errors

--- Classes/person.rb
+++ Classes/person.rb
@@ -1,10 +1,10 @@
 require 'securerandom'
 
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :parent_permission
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = SecureRandom.uuid
     @name = name
     @age = age
