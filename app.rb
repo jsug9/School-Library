999314fe75a792f1e9ea4b231f7ef1962647ee32
Augusto <jesus_augga_99@icloud.com>
2022-06-15 18:41:27 -0500

    add variables to app class

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
+require_relative './Classes/student'
+require_relative './Classes/teacher'
+require_relative './Classes/book'
+require_relative './Classes/rental'
+
 class App
-  def run
-    puts 'Hello, world!'
+  attr_reader :books, :people, :rentals
+
+  def initialize(books = [], people = [], rentals = [])
+    @books = books
+    @people = people
+    @rentals = rentals
   end
 end
