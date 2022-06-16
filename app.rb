b49e53e6be96bb157bba5c62bf1d5c943708b2a3
Augusto <jesus_augga_99@icloud.com>
2022-06-16 12:47:14 -0500

    add rental list function

--- app.rb
+++ app.rb
@@ -85,5 +85,18 @@ class App
     main_menu
   end
 
-  
+  def list_all_rentals_by_id
+    puts 'Select a person from the following list by number (not id)'
+    people.each_with_index do |person, index|
+      puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    print 'Enter the id of the person: '
+    person_selection = gets.chomp.to_i
+    puts 'Rentals:'
+    person = people[person_selection]
+    person.rentals.each do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+    end
+    main_menu
+  end
 end
