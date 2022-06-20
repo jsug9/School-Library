dd032b16dd9896c39e56b28be3463879461ac1c4
fed1k <allamurotovcoder@mail.ru>
2022-06-20 22:38:20 +0500

    create class for create_rental

--- MainClasses/create_rental.rb
+++ MainClasses/create_rental.rb
@@ -1,21 +1,23 @@
 require_relative '../Classes/rental'
 
-def create_rental(books, people, rentals)
-  puts 'Select a book from the following list by number'
-  books.each_with_index do |book, index|
-    puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+class CreateRental
+  def create_rental(books, people, rentals)
+    puts 'Select a book from the following list by number'
+    books.each_with_index do |book, index|
+      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+    end
+    book_selection = gets.chomp.to_i
+    puts
+    puts 'Select a person from the following list by number (not id)'
+    people.each_with_index do |person, index|
+      puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_selection = gets.chomp.to_i
+    puts ''
+    print('Enter the date of the rental (YYYY-MM-DD): ')
+    rental_date = gets.chomp
+    rental = Rental.new(rental_date, books[book_selection], people[person_selection])
+    rentals << rental
+    puts 'Rental created successfully'
   end
-  book_selection = gets.chomp.to_i
-  puts
-  puts 'Select a person from the following list by number (not id)'
-  people.each_with_index do |person, index|
-    puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-  end
-  person_selection = gets.chomp.to_i
-  puts ''
-  print('Enter the date of the rental (YYYY-MM-DD): ')
-  rental_date = gets.chomp
-  rental = Rental.new(rental_date, books[book_selection], people[person_selection])
-  rentals << rental
-  puts 'Rental created successfully'
 end
