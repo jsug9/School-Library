139536e54c08ac3197bf0b210a3eb562dd4b87ef
Augusto <jesus_augga_99@icloud.com>
2022-06-20 11:49:45 -0500

    make main.rb shorter

--- app.rb
+++ app.rb
@@ -12,6 +12,39 @@ class App
     @books = []
     @people = []
     @rentals = []
+    @status = true
+    @welcome_message = [
+      "Welcome to School Library App!\n ",
+      'Please choose an option by entering a number:',
+      '1 - List all books',
+      '2 - List all people',
+      '3 - Create a person',
+      '4 - Create a book',
+      '5 - Create a rental',
+      '6 - List all rentals for a given person id',
+      '7 - Exit'
+    ]
+  end
+
+  def run
+    while @status
+      @welcome_message.each { |i| puts i }
+      option = gets.chomp
+
+      case option
+      when '2' then list_all_people
+      when '1' then list_all_books
+      when '3' then create_person
+      when '4' then create_book
+      when '5' then create_rental
+      when '6' then list_all_rentals_by_id
+      when '7'
+        puts "Thank you for using this app!\n "
+        @status = false
+      else
+        puts "Sorry, you choose a wrong option\n "
+      end
+    end
   end
 
   # Lists files
--- main.rb
+++ main.rb
@@ -1,41 +1,9 @@
 #!/usr/bin/env ruby
 require_relative 'app'
 
-def main # rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength
+def main
   app = App.new
-
-  status = true
-
-  array = [
-    "Welcome to School Library App!\n ",
-     'Please choose an option by entering a number:',
-     '1 - List all books',
-     '2 - List all people',
-     '3 - Create a person',
-     '4 - Create a book',
-     '5 - Create a rental',
-     '6 - List all rentals for a given person id',
-     '7 - Exit'
-  ]
-
-  while status
-    array.each {|i| puts i }
-    option = gets.chomp
-
-    case option
-    when '1' then app.list_all_books
-    when '2' then app.list_all_people
-    when '3' then app.create_person
-    when '4' then app.create_book
-    when '5' then app.create_rental
-    when '6' then app.list_all_rentals_by_id
-    when '7'
-      puts "Thank you for using this app!\n "
-      status = false
-    else
-      puts "Sorry, you choose a wrong option\n "
-    end
-  end
+  app.run
 end
 
 main
