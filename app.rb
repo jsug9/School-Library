3a0f0219aef273dcc0d40e22e58427a68fe02931
fed1k <allamurotovcoder@mail.ru>
2022-06-20 22:22:31 +0500

    moving functions into separate files

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative './Classes/teacher'
 require_relative './Classes/book'
 require_relative './Classes/rental'
 require_relative './MainClasses/create_rental'
+require_relative './MainClasses/show_lists'
 require_relative 'get_values'
 
 class App
@@ -33,12 +34,12 @@ class App
       option = gets.chomp
 
       case option
-      when '2' then list_all_people
-      when '1' then list_all_books
+      when '1' then ShowLists.new.list_all_books(@books)
+      when '2' then ShowLists.new.list_all_people(@people)
       when '3' then create_person
       when '4' then create_book
       when '5' then create_rental(@books, @people, @rentals)
-      when '6' then list_all_rentals_by_id
+      when '6' then ShowLists.new.list_all_rentals_by_id(@people)
       when '7'
         puts "Thank you for using this app!\n "
         @status = false
@@ -46,15 +47,6 @@ class App
         puts "Sorry, you choose a wrong option\n "
       end
     end
-  end
-
-  # Lists files
-  def list_all_books
-    @books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
-  end
-
-  def list_all_people
-    @people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
   end
 
   # Create People file
@@ -97,20 +89,5 @@ class App
     book = Book.new(title, author)
     books << book unless @books.include?(book)
     puts 'Book created successfully'
-  end
-
-  # Lists files
-  def list_all_rentals_by_id
-    puts 'Select a person from the following list by number (not id)'
-    people.each_with_index do |person, index|
-      puts "#{index}) Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    print 'Enter the id of the person: '
-    person_selection = gets.chomp.to_i
-    puts 'Rentals:'
-    person = people[person_selection]
-    person.rentals.each do |rental|
-      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
-    end
   end
 end
