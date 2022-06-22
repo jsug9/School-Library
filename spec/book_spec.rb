ae70297e29257455fc49d908bb7c11840b42af40
Augusto <jesus_augga_99@icloud.com>
2022-06-22 15:19:43 -0500

    add book test

---
+++ spec/book_spec.rb
@@ -0,0 +1,19 @@
+require './Classes/book'
+
+describe Book do
+  title = 'The Great Gatsby'
+  author = 'F. Scott Fitzgerald'
+  let(:book) { Book.new(title, author) }
+
+  it 'should be a book' do
+    expect(book).to be_a(Book)
+  end
+
+  it 'should have a title' do
+    expect(book.title).to eq(title)
+  end
+
+  it 'should have an author' do
+    expect(book.author).to eq(author)
+  end
+end
