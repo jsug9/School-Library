0f3053e37225af1779bac0ec6e55c576e5bec5cc
fed1k <allamurotovcoder@mail.ru>
2022-06-23 03:37:18 +0500

    add tests to student and teacher

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,29 @@
+require './Classes/teacher'
+
+describe Teacher do
+  age = 35
+  name = 'Farhan'
+  specialization = 'YouTuber'
+  parent_permission = true
+  let(:teacher) {Teacher.new(age, specialization, name, parent_permission: parent_permission)}
+
+  it 'should have an age' do
+    expect(teacher.age).to be(age)
+  end
+
+  it 'should have a name' do
+    expect(teacher.name).to be(name)
+  end
+
+  it 'should have a specialization' do
+    expect(teacher.specialization).to be(specialization)
+  end
+
+  it 'should have a parent_permission' do
+    expect(teacher.parent_permission).to be(true)
+  end
+
+  it 'should be able to use services' do
+    expect(teacher.can_use_services?).to be(true)
+  end
+end
