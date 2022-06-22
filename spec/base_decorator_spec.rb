25109a1ff545a18bf42051cdd9809e92bae7558e
Augusto <jesus_augga_99@icloud.com>
2022-06-22 17:02:42 -0500

    add tests for base decorator

---
+++ spec/base_decorator_spec.rb
@@ -0,0 +1,21 @@
+require './Classes/Decorators/base_decorator'
+require './Classes/Decorators/nameable'
+require './Classes/person'
+
+describe BaseDecorator do
+  let(:person) { Person.new(18, 'Augusto') }
+  let(:nameable) { Nameable.new }
+  let(:base_decorator) { BaseDecorator.new(person) }
+
+  it 'should be a base decorator' do
+    expect(base_decorator).to be_a(BaseDecorator)
+  end
+
+  it 'has a correct_name method' do
+    expect(base_decorator.respond_to?(:correct_name)).to eq(true)
+  end
+
+  it 'correct_name equals the name of the person' do
+    expect(base_decorator.correct_name).to eq(person.name)
+  end
+end
---
+++ spec/capitalize_spec.rb
@@ -0,0 +1,6 @@
+# require './Classes/Decorators/capitalize_decorator'
+
+# describe CapitalizeDecorator do
+#   name = 'John Doe'
+#   let (:capitalize_decorator) { CapitalizeDecorator.new(nameable) }
+# end
